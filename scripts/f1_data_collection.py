ba047413d97139f5cede88bd233991d88d222ba0
Dafni <rosadafni0@gmail.com>
2024-08-03 21:36:53 -0300

    Add data + modified the collection

--- scripts/f1_data_collection.py
+++ scripts/f1_data_collection.py
@@ -1,15 +1,24 @@
 import pandas as pd
+import glob
 import os
 
 #Função que carrega os dados brutos
 
 def load_data():
-    data_folder = os.path.join('data')
-    file_name = 'drivers.csv'
-    file_path = os.path.join(data_folder, file_name)
-    df = pd.read_csv(file_path)
-    return df
+
+  #Listar todos os arquivos csv em um diretorio
+    csv_files = glob.glob('data/*.csv')
+
+  #Criar um novo dataframe para armazenar os dados de todos os arquivos
+    combined_df = pd.DataFrame()
+
+  
+  #Loop por cada arquivo CSV e anexe seu conteúdo ao dataframe combinado
+    for csv_file in csv_files:
+        df = pd.read_csv(csv_file)
+        combined_df = pd.concat([combined_df, df])
+
+    print(combined_df)
 
 if __name__ == "__main__":
-    df = load_data()
-    print(df.head())
+    load_data()
